97ae5f40b189c70feb3b48a39f93d9bc4c1fd4fa
Fahim Salam Chowdhury <fahim@e.email>
2025-08-11 16:25:41 +0600

    [fahim] feat: add custom nvim config

--- copy.sh
+++ copy.sh
@@ -461,7 +461,7 @@ fi
 
 printf "${INFO} - copying dotfiles ${SKY_BLUE}first${RESET} part\n"
 # Config directories which will ask the user whether to replace or not
-DIRS="ags fastfetch kitty rofi swaync alacritty khal vdirsyncer"
+DIRS="ags fastfetch kitty rofi swaync alacritty khal vdirsyncer nvim"
 
 for DIR2 in $DIRS; do
   DIRPATH="$HOME/.config/$DIR2"
