50a57a28113512fc0166dbada0c7c3e34c1bdafc
Fahim Salam Chowdhury <fahim@e.email>
2025-10-26 01:21:41 +0600

    [fahim]: add middle click option on waybar weather script to show wttr info also

---
+++ config/hypr/UserScripts/Weather-Toggle.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+state="$(cat ~/.config/hypr/UserScripts/Weather-State)"
+
+if [[ "$1" == "status" ]]; then
+	if [[ "$state" == "0" ]]; then
+		~/.config/hypr/UserScripts/Weather.py
+	else
+		wttrbar --ampm --observation-time
+	fi
+
+elif [[ "$1" == "toggle" ]]; then
+	if [[ "$state" == "0" ]]; then
+		echo '1' >~/.config/hypr/UserScripts/Weather-State
+	else
+		echo '0' >~/.config/hypr/UserScripts/Weather-State
+	fi
+
+	~/.config/hypr/scripts/Refresh.sh
+
+else
+	echo "Usage: $0 {status|toggle}"
+	exit 1
+fi
