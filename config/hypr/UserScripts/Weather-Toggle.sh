35f1aac2601a474c8fa02a9face170295840f54e
Fahim Salam Chowdhury <fahim@e.email>
2025-10-29 01:43:48 +0600

    [fahim] : fix : use custom wttrbar script on weather-toggle script

--- config/hypr/UserScripts/Weather-Toggle.sh
+++ config/hypr/UserScripts/Weather-Toggle.sh
@@ -3,22 +3,22 @@
 state="$(cat ~/.config/hypr/UserScripts/Weather-State)"
 
 if [[ "$1" == "status" ]]; then
-	if [[ "$state" == "0" ]]; then
-		~/.config/hypr/UserScripts/Weather.py
-	else
-		wttrbar --ampm --observation-time
-	fi
+  if [[ "$state" == "0" ]]; then
+    ~/.config/hypr/UserScripts/Weather.py
+  else
+    ~/.config/hypr/UserScripts/Wttrbar.py
+  fi
 
 elif [[ "$1" == "toggle" ]]; then
-	if [[ "$state" == "0" ]]; then
-		echo '1' >~/.config/hypr/UserScripts/Weather-State
-	else
-		echo '0' >~/.config/hypr/UserScripts/Weather-State
-	fi
+  if [[ "$state" == "0" ]]; then
+    echo '1' >~/.config/hypr/UserScripts/Weather-State
+  else
+    echo '0' >~/.config/hypr/UserScripts/Weather-State
+  fi
 
-	~/.config/hypr/scripts/Refresh.sh
+  ~/.config/hypr/scripts/Refresh.sh
 
 else
-	echo "Usage: $0 {status|toggle}"
-	exit 1
+  echo "Usage: $0 {status|toggle}"
+  exit 1
 fi
