41a27ac59d3c884c50183b8d8a105068ec321855
Fahim Salam Chowdhury <fahim@e.email>
2025-10-28 10:03:04 +0600

    [fahim] : fix distro updater
    - change term dependency for distro_updater from kitty to alackritty
    - add topgrade in the update list to update the packages on
    distro_update
    - at waybar, on notification group items, replace distro_dot_update with
    distro_update

--- config/hypr/scripts/Distro_update.sh
+++ config/hypr/scripts/Distro_update.sh
@@ -5,33 +5,36 @@
 # Local Paths
 iDIR="$HOME/.config/swaync/images"
 
-# Check for required tools (kitty)
-if ! command -v kitty &> /dev/null; then
-  notify-send -i "$iDIR/error.png" "Need Kitty:" "Kitty terminal not found. Please install Kitty terminal."
+# Check for required tools (Alacritty)
+if ! command -v alacritty &>/dev/null; then
+  notify-send -i "$iDIR/error.png" "Need Alacritty:" "Alacritty terminal not found. Please install Alacritty terminal."
   exit 1
 fi
 
 # Detect distribution and update accordingly
-if command -v paru &> /dev/null || command -v yay &> /dev/null; then
+if command -v topgrade &>/dev/null; then
+  alacritty --command topgrade -y
+  notify-send -i "$iDIR/ja.png" -u low 'System' 'has been updated.'
+elif command -v paru &>/dev/null || command -v yay &>/dev/null; then
   # Arch-based
-  if command -v paru &> /dev/null; then
-    kitty -T update paru -Syu
+  if command -v paru &>/dev/null; then
+    alacritty --command paru -Syu
     notify-send -i "$iDIR/ja.png" -u low 'Arch-based system' 'has been updated.'
   else
-    kitty -T update yay -Syu
+    alacritty --command yay -Syu
     notify-send -i "$iDIR/ja.png" -u low 'Arch-based system' 'has been updated.'
   fi
-elif command -v dnf &> /dev/null; then
+elif command -v dnf &>/dev/null; then
   # Fedora-based
-  kitty -T update sudo dnf update --refresh -y
+  alacritty --command sudo dnf update --refresh -y
   notify-send -i "$iDIR/ja.png" -u low 'Fedora system' 'has been updated.'
-elif command -v apt &> /dev/null; then
+elif command -v apt &>/dev/null; then
   # Debian-based (Debian, Ubuntu, etc.)
-  kitty -T update sudo apt update && sudo apt upgrade -y
+  alacritty --command sudo apt update && sudo apt upgrade -y
   notify-send -i "$iDIR/ja.png" -u low 'Debian/Ubuntu system' 'has been updated.'
-elif command -v zypper &> /dev/null; then
+elif command -v zypper &>/dev/null; then
   # openSUSE-based
-  kitty -T update sudo zypper dup -y
+  alacritty --command sudo zypper dup -y
   notify-send -i "$iDIR/ja.png" -u low 'openSUSE system' 'has been updated.'
 else
   # Unsupported distro
